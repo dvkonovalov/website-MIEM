ffd053ceda5f97c53a96feb14dfdab519a11f560
Yanis <yaabudiaf@miem.hse.ru>
2024-05-28 01:39:10 +0300

    Added things in the frontend

--- app/frontend/src/Components/NavBar.jsx
+++ app/frontend/src/Components/NavBar.jsx
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import useUserStatus from './UseUserStatus';
+import { ReactComponent as Logo } from '../Utils/logo.svg';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
   const { userStatus, isSessionValid, hasRemoteAccess } = useUserStatus();
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
 
 
   const handleLogOut = async () => {
@@ -34,13 +40,11 @@ const Navbar = () => {
   return (
     <nav className="bg-black text-white p-4">
       <div className="container mx-auto px-4 flex justify-between items-center">
-        {/* Logo on the left */}
-        <div className="text-xl font-bold">
+        <div>
           <a href="/" className="hover:text-gray-300">
             Logo
           </a>
         </div>
-        {/* Centered navigation links */}
         <div className="flex-grow flex flex-col sm:flex-row md:flex-row justify-center items-center space-x-0 sm:space-x-8 space-y-4 sm:space-y-0">
           <a href="/labs" className="hover:text-gray-300">Лабы</a>
           <a href="/material" className="hover:text-gray-300">Методички</a>
@@ -53,18 +57,36 @@ const Navbar = () => {
           )}
           <a href="/recrutement" className="hover:text-gray-300">Вакансии</a>
         </div>
-        {/* User info or Sign In button */}
+        <div className="relative inline-block text-left">
+      {isSessionValid ? (
         <div>
-          {isSessionValid ? (
-            <>
-              <span className="mr-4">{userStatus.username}</span>
-              <button onClick={handleLogOut} className="hover:text-gray-300">Log Out</button>
-            </>
-          ) : (
-            <button onClick={() => navigate('/signin')} className="hover:text-gray-300">Sign In</button>
+          <button
+            onClick={handleMenuToggle}
+            className="flex items-center mr-4 hover:text-gray-300 focus:outline-none"
+          >
+            {userStatus.username}
+          </button>
+          {menuOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-black border border-white">
+              <button
+                onClick={handleLogOut}
+                className="block w-full text-left px-4 py-2 hover:text-gray-300"
+              >
+                Выйти
+              </button>
+            </div>
           )}
         </div>
-      </div>
+      ) : (
+        <button
+          onClick={() => navigate('/signin')}
+          className="hover:text-gray-300"
+        >
+          Войти
+        </button>
+      )}
+    </div>
+    </div>
     </nav>
   );
 };
--- app/frontend/src/Components/Vacancy.jsx
+++ app/frontend/src/Components/Vacancy.jsx
@@ -21,31 +21,38 @@ const Vacancies = () => {
 
   
   return (
-    <div className="flex justify-center h-screen">
-      <div className="bg-black text-white p-4 w-3/4 md:4/5">
-        <div className="flex justify-start">
-        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center flex-1">Вакансии</h1>
-        </div>
-        <div className="flex flex-wrap justify-center gap-4 p-4">
-          {jobs.map((job) => (
-            <div key={job.id} className="flex flex-col w-48 md:w-64 md:h-72 md:h-96 p-2 border border-custom-gray justify-between">
-              <div>
-                <h3 className="text-2xl mb-2 font-bold">{job.title}</h3>
-                <ul className="mb-4">
-                  <li className="text-base font-semibold mb-2 mt-4">Навыки:</li>
-                  {job.skills.map((skill, index) => (
-                    <li className="text-sm" key={index}>• {skill}</li>
-                  ))}
-                </ul>
-              </div>
-              <button className="w-full h-12 text-white rounded-full border border-red-600 hover:border-red-700">
-                Подать заявку
-              </button>
-            </div>
-          ))}
-        </div>
+    <>
+    <div className="p-4">
+      <div className="mb-4 flex justify-between">
+        <a href="/" className="text-gray-400 hover:text-gray-300 text-lg sm:text-xl">
+          ← Главная
+        </a>
+        <h1 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center flex-1">Вакансии</h1>
+        <div style={{ width: '5.333333rem' }} /> {/* Matching width placeholder */}
       </div>
     </div>
+    <div className="flex justify-center h-screen">
+        <div className="bg-black text-white p-4 w-3/4 md:4/5">
+          <div className="flex flex-wrap justify-center gap-4 p-4">
+            {jobs.map((job) => (
+              <div key={job.id} className="flex flex-col w-48 md:w-64 md:h-72 md:h-96 p-2 border border-custom-gray justify-between">
+                <div>
+                  <h3 className="text-2xl mb-2 font-bold">{job.title}</h3>
+                  <ul className="mb-4">
+                    <li className="text-base font-semibold mb-2 mt-4">Навыки:</li>
+                    {job.skills.map((skill, index) => (
+                      <li className="text-sm" key={index}>• {skill}</li>
+                    ))}
+                  </ul>
+                </div>
+                <button className="w-full h-12 text-white rounded-full border border-red-600 hover:border-red-700">
+                  Подать заявку
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div></>
   );
 };
 
