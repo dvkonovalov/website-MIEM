afbf56adf18465449a3e8242a58b86244cc5cbf9
Daniil Konovalov <dvkonovalov@miem.hse.ru>
2024-05-30 22:45:52 +0300

    prod fix mobile

--- app/website/settings.py
+++ app/website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--l+0b#)lzc308vza)y=xbbz=w-^derbc^@@np!xc*1gt=9@3ka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '81.200.153.136']
 
